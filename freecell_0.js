e90448b43d0b872b3ab41509f0cb63f31f63ed7a
Theodore <theopaltoglou2@hotmail.gr>
2019-07-03 17:26:42 +0300

    update branch

---
+++ freecell_0.js
@@ -0,0 +1,9 @@
+
+//distinguish the game
+klondike = false
+freecell = true
+
+document.getElementById('element7').onload = function() {
+	loadAction();
+}
+
---
+++ solitaire_0.js
@@ -0,0 +1,9 @@
+
+//distinguish the game
+klondike = true
+freecell = false
+
+document.getElementById('element7').onload = function() {
+	loadAction();
+}
+
